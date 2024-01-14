7ba760b217885ed66fcecf9d6cf19087a2f6939b
Linus Aziegwu <linusaziegwu@gmail.com>
2024-01-14 20:06:01 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -27,7 +27,7 @@ function page() {
                 Join Us
               </button>
             </a>
-            <button
+  {/* <button
               data-collapse-toggle="navbar-sticky"
               type="button"
               class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -50,7 +50,7 @@ function page() {
                   d="M1 1h15M1 7h15M1 13h15"
                 />
               </svg>
-            </button>
+            </button> */}
           </div>
           <div
             class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
